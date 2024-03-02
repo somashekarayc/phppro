cf74003956f6f301e49b40c6f7d25199077e0ba6
som <somashekarayc@gmail.com>
2024-03-02 11:56:58 +0530

    ini

---
+++ admin/about.php
@@ -0,0 +1,17 @@
+<?php require_once '../config.php' ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin About</title>
+</head>
+<body>
+<a href="<?= URLROOT ?>/index">Home</a>
+<a href="<?= URLROOT ?>/about">about</a>
+<a href="<?= URLROOT ?>/admin/index">Admin Dashboard</a>
+<a href="<?= URLROOT ?>/admin/about">Admin About</a>
+</body>
+</html>
---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+//   // DB Params
+//   define("DB_HOST", "localhost");
+//   define("DB_USER", "root");
+//   define("DB_PASS", "");
+//   define("DB_NAME", "simple_mvc_project");
+
+//   // App Root
+//   define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://localhost/phppro');
+  // Site Name
+//   define('SITENAME', 'MvcProject');

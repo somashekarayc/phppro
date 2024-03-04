12da6ace92aca187277b33b5a632e0a894146394
som <somashekarayc@gmail.com>
2024-03-04 13:00:18 +0530

    2

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 //   define("DB_HOST", "localhost");
 //   define("DB_USER", "root");
 //   define("DB_PASS", "");
-//   define("DB_NAME", "simple_mvc_project");
+//   define("DB_NAME", "phppro");
 
 //   // App Root
 //   define('APPROOT', dirname(dirname(__FILE__)));

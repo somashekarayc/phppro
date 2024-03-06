08d11817a488f969f3ced0db46a233d3951bac5c
som <somashekarayc@gmail.com>
2024-03-06 12:29:47 +0530

    4

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,8 +1,4 @@
 
-<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-
-
 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
@@ -16,5 +12,42 @@
         </div>
     </div>
 </div>
+
+
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+
+
+
+<script>
+        var exampleModal = document.getElementById('exampleModal')
+    exampleModal.addEventListener('show.bs.modal', function(event) {
+        // Button that triggered the modal
+        var button = event.relatedTarget
+            // Extract info from data-bs-* attributes
+        var recipient = button.getAttribute('data-bs-whatever')
+        var url = button.getAttribute('data-url')
+        var size = button.getAttribute('data-size');
+        var modalTitle = exampleModal.querySelector('.modal-title')
+        var modalBodyInput = exampleModal.querySelector('.modal-body input')
+                modalTitle.textContent = recipient
+        $("#exampleModal .modal-dialog").addClass('modal-' + size);
+        $.ajax({
+            url: url,
+            success: function (data) {
+                $("#exampleModal").modal('hide');
+
+                $('#exampleModal .modal-body').html(data);
+                $("#exampleModal").modal('show');
+            },
+            error: function (data) {
+                data = data.responseJSON;
+                toastrs('Error', data.error, 'error')
+            }
+        });
+    })
+</script>
+
 </body>
 </html>

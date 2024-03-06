1deb70c716c7ad5424b0d0a079f270b5d33fcec5
som <somashekarayc@gmail.com>
2024-03-06 12:25:54 +0530

    1

--- about.php
+++ about.php
@@ -1,3 +1,23 @@
 <?= require_once './layouts/header.php' ?>
 
+<div class="container">
+    <input type="text" name="" id="input" class="form-controller">
+</div>
+
+<script>
+    console.log('cookieddd');
+
+    const cookie = name => `; ${document.cookie}`.split(`;
+${name}=`).pop().split(';').shift();
+    console.log(cookie('_ga'));
+    // Result: "GA1.2.1929736587.1601974046"
+
+
+    // $(document).ready(function () {
+    //     // var inputValue = $("#myInput").val();
+    //     const cookie = name => `; ${document.cookie}`.split(`;${name}=`).pop().split(';').shift();
+    //     $("#input").val(cookie('_ga'));
+    // });
+
+</script>
 <?= require_once './layouts/footer.php' ?>

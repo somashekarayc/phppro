473b42cb27e5f370270f746158da27faee444674
som <somashekarayc@gmail.com>
2024-03-06 12:29:04 +0530

    3

--- layouts/footer.php
+++ layouts/footer.php
@@ -2,5 +2,19 @@
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
+
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel"></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body body">
+
+            </div>
+        </div>
+    </div>
+</div>
 </body>
 </html>
